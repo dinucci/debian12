76dd042004ba1ef81e24c6b81620bb45debf6612
Ale DN <dinucci@protonmail.ch>
2024-01-08 02:12:04 +0100

    Update 0000.sh
    Replace <username> with your desired username. This script will perform the following tasks:
    
    Updates and upgrades all installed packages
    Installs essential tools (curl, wget, vim, ufw)
    Sets up the UFW firewall and allows OpenSSH traffic
    Creates a new user and adds them to the sudo group
    Configures SSH daemon to use key authentication instead of passwords
    Enables automatic security updates
    Sets the system time zone
    Installs Fail2Ban for brute force attack prevention
    Note that you should replace some values such as the timezone according to your need

--- 0000.sh
+++ 0000.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Replace <username> with your desired username. This script will perform the following tasks:
+
+# Updates and upgrades all installed packages
+# Installs essential tools (curl, wget, vim, ufw)
+# Sets up the UFW firewall and allows OpenSSH traffic
+# Creates a new user and adds them to the sudo group
+# Configures SSH daemon to use key authentication instead of passwords
+# Enables automatic security updates
+# Sets the system time zone
+# Installs Fail2Ban for brute force attack prevention
+# Note that you should replace some values such as the timezone according to your need
+
 # Update packages and install necessary tools
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y curl wget vim ufw
