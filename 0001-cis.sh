34808da42d40a74e490942d7ba68f6448e18c6a1
Ale DN <dinucci@protonmail.ch>
2025-11-19 09:38:41 +0100

    Update 0001-cis.sh
    updated
    
    sed -i "s#CIS_VERSIONS_DIR=.*#CIS_VERSIONS_DIR='$(pwd)'/versions#" /etc/default/cis-hardening

--- 0001-cis.sh
+++ 0001-cis.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Clone debian-cis repository
-git clone https://github.com/ovh/debian-cis.git
-cd debian-cis
+git clone https://github.com/ovh/debian-cis.git && cd debian-cis
 
 # Copy default configuration file and update paths accordingly
 cp debian/default /etc/default/cis-hardening
@@ -10,6 +9,7 @@ sed -i "s#CIS_LIB_DIR=.*#CIS_LIB_DIR='$(pwd)'/lib#" /etc/default/cis-hardening
 sed -i "s#CIS_CHECKS_DIR=.*#CIS_CHECKS_DIR='$(pwd)'/bin/hardening#" /etc/default/cis-hardening
 sed -i "s#CIS_CONF_DIR=.*#CIS_CONF_DIR='$(pwd)'/etc#" /etc/default/cis-hardening
 sed -i "s#CIS_TMP_DIR=.*#CIS_TMP_DIR='$(pwd)'/tmp#" /etc/default/cis-hardening
+sed -i "s#CIS_VERSIONS_DIR=.*#CIS_VERSIONS_DIR='$(pwd)'/versions#" /etc/default/cis-hardening
 
 # Auditing the specific check
 ./bin/hardening/1.1.1.1_disable_freevxfs.sh --audit
